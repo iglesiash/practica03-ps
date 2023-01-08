4043e702fc20466cfae022444e4cc6b694be15d7
Héctor Iglesias <hgi834@alumnos.unican.es>
2023-01-08 02:19:22 +0100

    Añadidas interfaces que no estaban en el anterior commit

--- UCParkCommon/src/main/java/es/unican/ps/practica03/business/IReportLocal.java
+++ UCParkCommon/src/main/java/es/unican/ps/practica03/business/IReportLocal.java
@@ -1,11 +1,7 @@
 package es.unican.ps.practica03.business;
 
-import es.unican.ps.practica03.model.Report;
-import es.unican.ps.practica03.model.Vehicle;
 import jakarta.ejb.Local;
 
 @Local
-public interface IReportLocal {
-	public void reportParking(Vehicle vehicle, String description, double price);
-	public void voidReport(Report report);
+public interface IReportLocal extends IReport {
 }
--- UCParkCommon/src/main/java/es/unican/ps/practica03/business/IUserLocal.java
+++ UCParkCommon/src/main/java/es/unican/ps/practica03/business/IUserLocal.java
@@ -1,14 +1,7 @@
 package es.unican.ps.practica03.business;
 
-import java.util.List;
-
-import es.unican.ps.practica03.model.Parking;
-import es.unican.ps.practica03.model.Report;
 import jakarta.ejb.Local;
 
 @Local
 public interface IUserLocal {
-	public List<Report> consultReports(String email);
-	public List<Parking> consultCurrentParkingList(String email);
-	public List<Parking> consultParkingHistory(String email);
 }
